7e881a5fbd64ca24d4f48aed8df5eb567714c166
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2024-01-12 05:43:18 +0000

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Floor function Module"""
+
+
+def floor(n: float) -> int:
+    """Syntax: float(n)
+
+    args:
+        n (float): Any float
+
+    return (int): floor of n"""
+    return int(n)
