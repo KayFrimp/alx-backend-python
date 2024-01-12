98dc254111537888b39d1fa8fbf9af61ccd922c0
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2024-01-12 05:40:05 +0000

    concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,11 +4,11 @@
 
 def concat(str1: str, str2: str) -> str:
     """Syntax: concat(str1, str2)
-    
+
     args:
         str1 (string): first string
         str2 (string): second string
-        
+
     return (string): concatenates str1 and str2
     """
     return str1 + str2
