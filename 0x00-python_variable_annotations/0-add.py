74c8ad705e68c802b723969109dd981131f35c84
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2024-01-12 05:31:40 +0000

    Basic annotations-add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Addition Module"""
+
+
+def add(a: float, b: float) -> float:
+    """Function adds two floats, a and b
+    and returns the sum as a float"""
+    return a + b
