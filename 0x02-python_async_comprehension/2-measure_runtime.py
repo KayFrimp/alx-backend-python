56346a4abcdccde5b128ad7e9b0e4ebe498d84cc
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2024-01-20 01:43:41 +0000

    coroutines with async comprehensions

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/bin/bash/env python3
+"""measure_runtime Module"""
+
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Executes async_comprehension four times in parallel using
+    asyncio.gather and measures total runtime."""
+    start_time = time.perf_counter()
+    awaitables = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*awaitables)
+    end_time = time.perf_counter()
+    return end_time - start_time
