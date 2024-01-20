8f157d92bff5627f41b7738c1b6e9e1e102f63b6
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2024-01-20 01:54:56 +0000

    fixed type annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Loops 10 times and yields random number asynchronously"""
     for _ in range(10):
         await asyncio.sleep(1)
